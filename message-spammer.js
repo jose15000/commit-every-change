81d1b2f76d41ce21e214ec02d71063b0434f5b34
Jose Henrique <jose15000henrique@outlook.com>
2024-09-09 14:30:12 -0300

    script de spam de mensagems no wpp

---
+++ message-spammer.js
@@ -0,0 +1,29 @@
+async function sendMessage(scriptText, repeatCount) {
+  const lines = Array(repeatCount).fill(scriptText);
+  (main = document.querySelector("#main")),
+    (textarea = main.querySelector(`div[contenteditable="true"]`));
+
+  if (!textarea) throw new Error("Não há uma conversa aberta");
+
+  for (const line of lines) {
+    console.log(line);
+
+    textarea.focus();
+    document.execCommand("insertText", false, line);
+    textarea.dispatchEvent(new Event("change", { bubbles: true }));
+
+    setTimeout(() => {
+      (
+        main.querySelector(`[data-testid="send"]`) ||
+        main.querySelector(`[data-icon="send"]`)
+      ).click();
+    }, 100);
+
+    if (lines.indexOf(line) !== lines.length - 1)
+      await new Promise((resolve) => setTimeout(resolve, 250));
+  }
+
+  return lines.length;
+}
+
+sendMessage("huehue", 1000000);
